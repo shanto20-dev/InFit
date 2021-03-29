41d655f7eff6655ecdb1e8401aa27a55d23d8ed3
Parth <pcs003@ucsd.edu>
2021-03-29 11:43:49 -0400

    Add landing page files and test image for heroku

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom'
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import NavbarContainer from "./nav/navbar_container";
+import LandingPage from './landing_page/landing_page'
 
 const App = () => (
   <div>
     <NavbarContainer />
     <Switch>
+      <Route path="/" component={LandingPage}/>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
---
+++ frontend/src/components/landing_page/landing_page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Logo from "../../assets/InfitLogo.png"
+
+export default class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={Logo}/>
+            </div>
+        )
+    }
+}
