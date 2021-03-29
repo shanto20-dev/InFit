48422660cf59513274715bf95c4fcb530bc037ae
shanto20-dev <ecshanto@gmail.com>
2021-03-29 13:24:40 -0400

    complete clothing actions and clothing reducer

--- frontend/src/actions/clothing_actions.js
+++ frontend/src/actions/clothing_actions.js
@@ -3,38 +3,46 @@ import { createClothing, fetchAllClothing, fetchUserClothing, deleteClothing } f
 export const RECEIVE_ALL_CLOTHING = "RECEIVE_ALL_CLOTHING";
 export const RECEIVE_USER_CLOTHING = "RECEIVE_USER_CLOTHING";
 export const RECEIVE_CLOTHING = "RECEIVE_CLOTHING";
+export const REMOVE_CLOTHING = "REMOVE_CLOTHING";
 
-export const receiveAllClothing = (clothings) => ({
+const receiveAllClothing = (clothings) => ({
   type: RECEIVE_ALL_CLOTHING,
   clothings,
 });
 
-export const receiveUserClothing = (clothings) => ({
+const receiveUserClothing = (clothings) => ({
   type: RECEIVE_USER_CLOTHING,
   clothings,
 });
 
-export const receiveClothing = (clothing) => ({
+const receiveClothing = (clothing) => ({
   type: RECEIVE_CLOTHING,
   clothing,
 });
 
-export const receiveClothing = (clothing) => ({
-  type: RECEIVE_CLOTHING,
-  clothing,
+const removeClothing = (clothingId) => ({
+  type: REMOVE_CLOTHING,
+  clothingId,
 });
 
-export const fetchTweets = () => (dispatch) =>
-  getTweets()
-    .then((tweets) => dispatch(receiveTweets(tweets)))
-    .catch((err) => console.log(err));
+export const getAllClothing = () => (dispatch) =>
+  fetchAllClothing()
+    .then((clothings) => dispatch(receiveAllClothing(clothings)), (err) => console.log(err))
 
-export const fetchUserTweets = (id) => (dispatch) =>
-  getUserTweets(id)
-    .then((tweets) => dispatch(receiveUserTweets(tweets)))
-    .catch((err) => console.log(err));
+export const getUserClothing = (id) => (dispatch) =>
+  fetchUserClothing(id)
+    .then((clothings) => dispatch(receiveUserClothing(clothings)), (err) => console.log(err));
 
-export const composeTweet = (data) => (dispatch) =>
-  writeTweet(data)
-    .then((tweet) => dispatch(receiveNewTweet(tweet)))
-    .catch((err) => console.log(err));
+export const newClothing = (clothingData) => (dispatch) =>
+  createClothing(clothingData)
+    .then((clothing) => dispatch(receiveClothing(clothing)), (err) => console.log(err));
+
+    export const newClothing = (clothingData) => (dispatch) =>
+  createClothing(clothingData)
+    .then((clothing) => dispatch(receiveClothing(clothing)), (err) => console.log(err));
+
+export const deleteClothing = (clothingId) => (dispatch) =>
+  deleteClothing(clothingId)
+    .then(() => dispatch(removeClothing(clothingId)), (err) => console.log(err));
+
+    
