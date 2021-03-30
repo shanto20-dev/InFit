3ef949768cb0f717b48c4fa780370f759bf7af76
Parth <pcs003@ucsd.edu>
2021-03-30 12:38:59 -0400

    start closet page functionality

---
+++ frontend/src/components/closet/outfits.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Outfits extends Component {
+    render() {
+        return (
+            <div>
+                Outfits
+            </div>
+        )
+    }
+}
