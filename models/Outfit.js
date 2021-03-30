a4518d25d7c6f07e751398cc421d254974b1421e
Ariton Sefedini <aritonsefedini@icloud.com>
2021-03-30 10:22:23 -0400

    Added outfit model, validations, and routes

---
+++ models/Outfit.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OutfitSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+    },
+    name: {
+        type: string,
+        required: true,
+    },
+    tags: {
+        type: Array,
+    },
+    description: {
+        type: String,
+    },
+    img_url: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+module.exports = Outfit = mongoose.model("Outfit", OutfitSchema)
