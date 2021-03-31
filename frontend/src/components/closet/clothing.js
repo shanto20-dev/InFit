d51da5bc77b51988ee344f07b25dee003e293fea
Parth <pcs003@ucsd.edu>
2021-03-31 10:12:52 -0400

    added tag/category filter on closet clothing page

--- frontend/src/components/closet/clothing.js
+++ frontend/src/components/closet/clothing.js
@@ -8,10 +8,15 @@ export default class Clothing extends Component {
         super(props)
 
         this.state = {
-            currentPage: 0
+            currentPage: 0,
+            categoryFilter: "",
+            tagFilter: ""
         }
 
-        this.setPage = this.setPage.bind(this)
+        this.setPage = this.setPage.bind(this);
+        this.setCategory = this.setCategory.bind(this);
+        this.setTag = this.setTag.bind(this)
+        this.getAllTags = this.getAllTags.bind(this);
     }
 
     setPage(val) {
@@ -29,10 +34,46 @@ export default class Clothing extends Component {
         }
     }
 
+    setCategory(e) {
+        e.preventDefault();
+        this.setState({
+            categoryFilter: e.currentTarget.value   
+        })
+    }
+
+    setTag(e) {
+        e.preventDefault();
+        this.setState({
+            tagFilter: e.currentTarget.value   
+        })
+    }
+
+    getAllTags() {
+        let allTags = [];
+        Object.values(this.props.clothing).forEach(cloth => {
+            cloth.tags.forEach(tag => {
+                if (!allTags.includes(tag)) {
+                    allTags.push(tag)
+                }
+            })
+        })
+        return allTags;
+    }
+
     render() {
         let start = this.state.currentPage * 20;
         let end = this.state.currentPage * 20 + 20;
-        let clothingElements = Object.values(this.props.clothing).slice(start, end).map((cloth, i) => {
+        
+        let clothingElements = Object.values(this.props.clothing)
+        .filter( cloth => cloth.category.includes(this.state.categoryFilter))
+        .filter(cloth => {
+            if (this.state.tagFilter === ""){
+                return true;
+            } else {
+                return cloth.tags.includes(this.state.tagFilter)
+            }
+        })
+        .slice(start, end).map((cloth, i) => {
             let clothUrl = `/clothing/${cloth._id}`;
             return (
                 <div className="clothing-item" key={i}>
@@ -53,10 +94,43 @@ export default class Clothing extends Component {
             )
         }
 
+        let allTags = this.getAllTags();
+        let tagOptions = allTags.map(tag => {
+            return (
+                <option value={tag}>{tag}</option>
+            )
+        })
+        
         return (
             <div className="clothing-wrapper">
                 <div className="header-div">
                     <h1>Your Clothing</h1>
+                    <div className="select-div">
+                        <select 
+                            name="category"
+                            defaultValue=""
+                            onChange={this.setCategory}
+                            className="category-dropdown field"
+                        >
+                            <option value="">All Categories</option>
+                            <option value="Top">Top</option>
+                            <option value="Bottom">Bottom</option>
+                            <option value="Dress">Dress</option>
+                            <option value="Outerwear">Outerwear</option>
+                            <option value="Undergarment">Undergarment</option>
+                            <option value="Accessory">Accessory</option>
+                            <option value="Shoes">Shoes</option>
+                        </select>
+                        <select 
+                            name="tag"
+                            defaultValue=""
+                            onChange={this.setTag}
+                            className="tag-dropdown field"
+                        >
+                            <option value="">All Tags</option>
+                            {tagOptions}
+                        </select>
+                    </div>
                 </div>
                 
                 <div className="clothing-list">
