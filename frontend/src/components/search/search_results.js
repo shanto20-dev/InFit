f12c1ced7e1d096be4b38974b514a4b8b3a2247d
Lakhte <lakhteagha@gmail.com>
2021-04-01 17:37:07 -0400

    styling and filtering

--- frontend/src/components/search/search_results.js
+++ frontend/src/components/search/search_results.js
@@ -8,62 +8,104 @@ export default class SearchResults extends Component {
 
         this.detailsRef = [];
         this.cardRef = [];
+        this.state = { filter: "all" };
+
+        this.update = this.update.bind(this);
+    }
+
+    update(e) {
+        this.setState({ filter: e.target.value });
     }
 
     render() {
+        let filtered;
+        this.state.filter === "all"
+            ? (filtered = this.props.searchResults)
+            : this.state.filter === "clothing"
+            ? (filtered = this.props.searchResults.filter(
+                  (result) => !result.clothes
+              ))
+            : (filtered = this.props.searchResults.filter(
+                  (result) => result.clothes
+              ));
         return (
-            <div className="search-results-container">
-                {this.props.searchResults.map((result, idx) => {
-                    let link;
-                    if (result.clothes) {
-                        link = `/outfit/${result._id}`;
-                    } else {
-                        link = `clothing/${result._id}`;
-                    }
+            <div>
+                <div className="filters-bar-container">
+                    <select
+                        className="filters-bar"
+                        name="filter"
+                        id=""
+                        defaultValue="all"
+                        onChange={this.update}
+                    >
+                        <option value="all">All</option>
+                        <option value="clothing">Clothing</option>
+                        <option value="outfits">Outfits</option>
+                    </select>
+                </div>
+                <div className="search-results-container">
+                    {filtered.map((result, idx) => {
+                        let link;
+                        if (result.clothes) {
+                            link = `/outfit/${result._id}`;
+                        } else {
+                            link = `clothing/${result._id}`;
+                        }
 
-                    return (
-                        <Link
-                            className="link"
-                            key={idx}
-                            ref={(card) => (this.cardRef[idx] = card)}
-                            to={link}
-                        >
-                            <div
-                                onMouseEnter={() => {
-                                    this.detailsRef[idx].classList.remove(
-                                        "hidden"
-                                    );
-                                }}
-                                onMouseLeave={() => {
-                                    this.detailsRef[idx].classList.add(
-                                        "hidden"
-                                    );
-                                }}
-                                className="search-result-card"
+                        return (
+                            <Link
+                                className="link"
+                                key={idx}
+                                ref={(card) => (this.cardRef[idx] = card)}
+                                to={link}
                             >
-                                <img
-                                    className="search-result-img"
-                                    src={result.img_url}
-                                />
-                                <div className="overlay"></div>
                                 <div
-                                    ref={(details) =>
-                                        (this.detailsRef[idx] = details)
-                                    }
-                                    className="details hidden"
+                                    onMouseEnter={() => {
+                                        this.detailsRef[idx].classList.remove(
+                                            "hidden"
+                                        );
+                                    }}
+                                    onMouseLeave={() => {
+                                        this.detailsRef[idx].classList.add(
+                                            "hidden"
+                                        );
+                                    }}
+                                    className="search-result-card"
                                 >
-                                    <h1 className="search-result-name">
-                                        {result.name}
-                                    </h1>
+                                    <img
+                                        className="search-result-img"
+                                        src={result.img_url}
+                                    />
+                                    <div className="overlay"></div>
+                                    <div
+                                        ref={(details) =>
+                                            (this.detailsRef[idx] = details)
+                                        }
+                                        className="details hidden"
+                                    >
+                                        <h1 className="search-result-name">
+                                            {result.name}
+                                        </h1>
 
-                                    <h1 className="search-result-category">
-                                        {result.category}
-                                    </h1>
+                                        <h1 className="search-result-category">
+                                            {result.category}
+                                        </h1>
+
+                                        <h1 className="search-result-tags">
+                                            {result.tags.map((tag, idx) => {
+                                                return (
+                                                    <span key={idx}>
+                                                        {tag}{" "}
+                                                    </span>
+                                                );
+                                            })}
+                                        </h1>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    );
-                })}
+                            </Link>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
