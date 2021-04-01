af1084ebd0ca33b1e8d301d88cbab8a56cd95303
Lakhte <lakhteagha@gmail.com>
2021-04-01 11:51:33 -0400

    bar & results

--- frontend/src/actions/search_actions.js
+++ frontend/src/actions/search_actions.js
@@ -3,15 +3,14 @@ import { searchClothingByName } from "../util/search_api_util";
 export const RECEIVE_SEARCHED_CLOTHING = "RECEIVE_SEARCHED_CLOTHING";
 
 const receiveSearchedClothing = (clothing) => {
-  console.log(clothing);
-  return {
-    type: RECEIVE_SEARCHED_CLOTHING,
-    clothing,
-  };
+    return {
+        type: RECEIVE_SEARCHED_CLOTHING,
+        clothing,
+    };
 };
 
 export const searchClothesByName = (searchTerm) => (dispatch) => {
-  return searchClothingByName(searchTerm)
-    .then((clothing) => dispatch(receiveSearchedClothing(clothing.data)))
-    .catch((clothing) => console.log(clothing));
+    return searchClothingByName(searchTerm)
+        .then((clothing) => dispatch(receiveSearchedClothing(clothing.data)))
+        .catch((clothing) => console.log(clothing));
 };
--- frontend/src/components/search/search_results.js
+++ frontend/src/components/search/search_results.js
@@ -1,13 +1,63 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../../styles/search/search_results.css";
 
 export default class SearchResults extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.searchResults.map((result) => {
-          return <div>{result.name}</div>;
-        })}
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.detailsRef = [];
+        this.cardRef = [];
+    }
+
+    render() {
+        return (
+            <div className="search-results-container">
+                {this.props.searchResults.map((result, idx) => {
+                    return (
+                        <Link
+                            className="link"
+                            key={idx}
+                            ref={(card) => (this.cardRef[idx] = card)}
+                            to={`/clothing/${result._id}`}
+                        >
+                            <div
+                                onMouseEnter={() => {
+                                    this.detailsRef[idx].classList.remove(
+                                        "hidden"
+                                    );
+                                }}
+                                onMouseLeave={() => {
+                                    this.detailsRef[idx].classList.add(
+                                        "hidden"
+                                    );
+                                }}
+                                className="search-result-card"
+                            >
+                                <img
+                                    className="search-result-img"
+                                    src={result.img_url}
+                                />
+                                <div className="overlay"></div>
+                                <div
+                                    ref={(details) =>
+                                        (this.detailsRef[idx] = details)
+                                    }
+                                    className="details hidden"
+                                >
+                                    <h1 className="search-result-name">
+                                        {result.name}
+                                    </h1>
+
+                                    <h1 className="search-result-category">
+                                        {result.category}
+                                    </h1>
+                                </div>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+        );
+    }
 }
