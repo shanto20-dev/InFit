98ae79fae010acf90ebbed4eee824439d5117ad0
Parth <pcs003@ucsd.edu>
2021-04-02 10:32:47 -0400

    Modify closet button

--- frontend/src/components/clothing/clothing_show_container.js
+++ frontend/src/components/clothing/clothing_show_container.js
@@ -1,10 +1,12 @@
 import { connect } from "react-redux";
 import { getClothing } from "../../actions/clothing_actions";
+import { currentUser } from "../../util/session_api_util";
 import ClothingShow from "./clothing_show";
 
 const mapStateToProps = (state) => {
   return {
     clothing: state.entities.clothing,
+    currentUser: currentUser,
   };
 };
 
