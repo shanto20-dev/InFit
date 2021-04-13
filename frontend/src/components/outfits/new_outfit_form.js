cf4efd13c6434da92d51b6fad769a0df21374b8d
Lakhte <lakhteagha@gmail.com>
2021-04-13 10:06:06 -0400

    errors for new outfit form

--- frontend/src/components/outfits/new_outfit_form.js
+++ frontend/src/components/outfits/new_outfit_form.js
@@ -2,128 +2,145 @@ import React from "react";
 import "../../styles/outfits/new_outfit_form.css";
 
 class NewOutfitForm extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      currentUser: { id: "", username: "", email: "" },
-      name: "",
-      tags: [],
-      description: "",
-      img_url: "",
-      tagText: "",
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.addTag = this.addTag.bind(this)
-    this.removeTag = this.removeTag.bind(this)
-  }
+        this.state = {
+            currentUser: { id: "", username: "", email: "" },
+            name: "",
+            tags: [],
+            description: "",
+            img_url: "",
+            tagText: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.addTag = this.addTag.bind(this);
+        this.removeTag = this.removeTag.bind(this);
+    }
 
-  componentDidMount() {
-    this.props.currentUser().then((result) => {
-      this.setState({
-        currentUser: result.data,
-      });
-    });
-  }
+    componentDidMount() {
+        this.props.currentUser().then((result) => {
+            this.setState({
+                currentUser: result.data,
+            });
+        });
+    }
 
-  handleSubmit(e) {
-    e.preventDefault();
+    handleSubmit(e) {
+        e.preventDefault();
 
-    this.props
-      .newOutfit(this.state)
-      .then((action) => {
-        this.props.history.push(`/outfit/${action.outfit.data._id}`)
-      }
-      );
-  }
+        this.props.newOutfit(this.state).then((action) => {
+            if (action.type !== "RECEIVE_SESSION_ERRORS")
+                this.props.history.push(`/outfit/${action.outfit.data._id}`);
+        });
+    }
 
-  update(type) {
-    return (e) =>
-      this.setState({
-        [type]: e.currentTarget.value,
-      });
-  }
+    update(type) {
+        return (e) =>
+            this.setState({
+                [type]: e.currentTarget.value,
+            });
+    }
 
-  addTag(e) {
-    e.preventDefault();
-    if (this.state.tagText === "") return;
-    if (this.state.tags.includes("#" + this.state.tagText)) return;
-    let current = this.state.tags;
-    current.push("#" + this.state.tagText);
-    this.setState({
-      tags: current,
-      tagText: ""
-    })
-  }
+    addTag(e) {
+        e.preventDefault();
+        if (this.state.tagText === "") return;
+        if (this.state.tags.includes("#" + this.state.tagText)) return;
+        let current = this.state.tags;
+        current.push("#" + this.state.tagText);
+        this.setState({
+            tags: current,
+            tagText: "",
+        });
+    }
 
-  removeTag(e) {
-    e.preventDefault();
-    let current = this.state.tags;
-    current.splice(e.currentTarget.id, 1)
-    this.setState({
-      tags: current
-    })
-  }
+    removeTag(e) {
+        e.preventDefault();
+        let current = this.state.tags;
+        current.splice(e.currentTarget.id, 1);
+        this.setState({
+            tags: current,
+        });
+    }
 
-  render() {
+    renderErrors() {
+        return (
+            <ul>
+                {Object.keys(this.props.errors).map((error, i) => (
+                    <li key={`error-${i}`}>{this.props.errors[error]}</li>
+                ))}
+            </ul>
+        );
+    }
 
-    let currentTags = this.state.tags.map((tag, i) => {
-      return (
-        <span key={i} className="tag">{tag}<span className="delete-tag" id={i} onClick={this.removeTag}>X</span></span>
-      )
-    })
+    render() {
+        let currentTags = this.state.tags.map((tag, i) => {
+            return (
+                <span key={i} className="tag">
+                    {tag}
+                    <span
+                        className="delete-tag"
+                        id={i}
+                        onClick={this.removeTag}
+                    >
+                        X
+                    </span>
+                </span>
+            );
+        });
+        console.log(this.props.errors);
+        return (
+            <div className="new-outfit-form-container">
+                <form className="new-outfit-form" onSubmit={this.handleSubmit}>
+                    <h1 className="new-outfit-form-header">
+                        Add a new outfit to your closet
+                    </h1>
+                    <label>Outfit Name</label>
+                    <input
+                        type="text"
+                        value={this.state.name}
+                        onChange={this.update("name")}
+                        placeholder="Outfit Name"
+                        className="new-outfit-name"
+                    />
 
-    return (
-      <div className="new-outfit-form-container">
-        <form className="new-outfit-form" onSubmit={this.handleSubmit}>
-          <h1 className="new-outfit-form-header">
-            Add a new outfit to your closet
-          </h1>
-          <label>Outfit Name</label>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.update("name")}
-            placeholder="Outfit Name"
-            className="new-outfit-name"
-          />
+                    <label>Tags</label>
+                    <div className="tags-div">
+                        <input
+                            type="text"
+                            className="new-outfit-tags field"
+                            value={this.state.tagText}
+                            onChange={this.update("tagText")}
+                            placeholder="#tag"
+                        />
+                        <span className="add-tag" onClick={this.addTag}>
+                            +
+                        </span>
+                    </div>
+                    <div className="current-tags">{currentTags}</div>
 
-          <label>Tags</label>
-          <div className = "tags-div">
-            <input
-              type="text"
-              className="new-outfit-tags field"
-              value={this.state.tagText}
-              onChange={this.update("tagText")}
-              placeholder="#tag"
-            />
-            <span className="add-tag" onClick={this.addTag}>+</span>
-          </div>
-          <div className="current-tags">
-            {currentTags}
-          </div>
+                    <label>Description</label>
+                    <textarea
+                        className="new-outfit-description"
+                        value={this.state.description}
+                        onChange={this.update("description")}
+                        placeholder="Description"
+                    />
 
-          <label>Description</label>
-          <textarea
-            className="new-outfit-description"
-            value={this.state.description}
-            onChange={this.update("description")}
-            placeholder="Description"
-          />
-
-          <label>Image</label>
-          <input
-            type="text"
-            className="new-outfit-img_url"
-            value={this.state.img_url}
-            onChange={this.update("img_url")}
-            placeholder="Upload an Image"
-          />
-          <button>Create Outfit</button>
-        </form>
-      </div>
-    );
-  }
+                    <label>Image</label>
+                    <input
+                        type="text"
+                        className="new-outfit-img_url"
+                        value={this.state.img_url}
+                        onChange={this.update("img_url")}
+                        placeholder="Upload an Image"
+                    />
+                    {this.renderErrors()}
+                    <button>Create Outfit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default NewOutfitForm;
